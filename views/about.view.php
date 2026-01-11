d6a422e716f2ff9d7ca98380161cec1b501efd93
BDragon <baolong2k6iter@gmail.com>
2026-01-11 16:10:54 +0700

    Ep 13 - Partials

---
+++ views/about.view.php
@@ -0,0 +1,12 @@
+<?php require 'partials/head.view.php' ?>
+<div class="min-h-full">
+    <?php require 'partials/nav.view.php' ?>
+    <?php require 'partials/banner.view.php' ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            About me
+        </div>
+    </main>
+</div>
+<?php require 'partials/foot.view.php' ?>
---
+++ views/partials/foot.view.php
@@ -0,0 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+</body>
+
+</html>
