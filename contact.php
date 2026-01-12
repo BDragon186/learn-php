b870070735d3ca173d60a8747af81be016f41be9
BDragon <baolong2k6iter@gmail.com>
2026-01-12 18:10:04 +0700

    Ep 14 - Superglobals and Current Page Styling

--- contact.php
+++ contact.php
@@ -1,2 +1,5 @@
 <?php
+require 'functions.php';
+$heading = "contact";
+// echo $_SERVER["REQUEST_URI"];
 require 'views/contact.view.php';
--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
+require 'functions.php';
+$heading = "home";
+
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+echo __DIR__;
+// echo $_SERVER["REQUEST_URI"];
 require 'views/index.view.php';
